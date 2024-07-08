58d69136b844dcb1f6161647dea22ead16d9952a
Rhodes Steven Surell <rhodes.surell@mims.com>
2024-07-08 21:49:15 +0800

    Inital Commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,29 +5,6 @@ class Program
 {
     static void Main()
     {
-        string folderPath = $@"C:\DigitalOutput_AU\WS_Working\{DateTime.Now.ToString("yyyyMM")}00\";
-        string fileName = "hello_world.txt";
-        string filePath = Path.Combine(folderPath, fileName);
-
-        try
-        {
-            // Check if the directory exists, if not, create it.
-            if (!Directory.Exists(folderPath))
-            {
-                Directory.CreateDirectory(folderPath);
-            }
-
-            // Write to the file
-            using (StreamWriter writer = new StreamWriter(filePath))
-            {
-                writer.WriteLine("Hello, World!");
-            }
-
-            Console.WriteLine("File created successfully at: " + filePath);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("An error occurred: " + ex.Message);
-        }
+        Console.WriteLine("Hello World");
     }
 }
